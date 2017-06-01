3fa586a41c1bd79c97c016afb0900b2bd9847deb
Philippe Hürlimann <p@hurlimann.org>
2017-06-01 11:02:43 +0200

    handle hash change

--- src/AppRunner.js
+++ src/AppRunner.js
@@ -1,13 +1,18 @@
 "use strict"
 let taskList = new TaskList("Pendenzenliste: ")
-$(() => {
+
+function loadTaskListFromHash() {
     let id = location.hash.substring(location.hash.indexOf('#') + 1)
     if (id) {
-        $.getJSON(REMOTE_BASE + id, (data) => {
+        $.getJSON(REMOTE_BASE + id, data => {
             taskList = new TaskList(data)
             updateView()
         })
     } else {
         updateView()
     }
+}
+$(() => {
+    $(window).on("hashchange", loadTaskListFromHash)
+    loadTaskListFromHash()
 })
